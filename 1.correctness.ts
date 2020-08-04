2a09aa9bdcfcd02bb95e03c733851486a0845fc6
Sivabudh Umpudh <sivabudh.u@codium.co>
2020-08-05 00:45:49 +0700

    First question

---
+++ 1.correctness.ts
@@ -0,0 +1,10 @@
+/*
+Question #1: Correctness
+
+In TypeScript, write a function called "scriptAt". Prove that it's correct by providing a set of test cases. 
+The function will do as follows.
+
+Given a string as an input, find the index of the string "Script" within the input string. 
+
+You can use Google to look up the method calls.
+*/
